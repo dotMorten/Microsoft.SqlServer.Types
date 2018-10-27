bc0a0973e8f3083826289e2f1ea983bb6f8edc66
Morten Nielsen <no@spam.thx>
2018-10-27 13:28:31 -0700

    Fix indents

--- src/Microsoft.SqlServer.Types/Wkt/WktWriter.cs
+++ src/Microsoft.SqlServer.Types/Wkt/WktWriter.cs
@@ -12,34 +12,34 @@ namespace Microsoft.SqlServer.Types
     /// </summary>
     internal static class WktWriter
     { 
-		/// <summary>
-		/// Writes a <see cref="Geometry"/> instance as Well-Known Text according to the OGC Simple Features Specification 1.2.0.
-		/// </summary>
-		/// <param name="g">Geometry</param>
-		/// <returns>Well-Known Text</returns>
-		public static string Write(ShapeData g)
-		{
+        /// <summary>
+        /// Writes a <see cref="Geometry"/> instance as Well-Known Text according to the OGC Simple Features Specification 1.2.0.
+        /// </summary>
+        /// <param name="g">Geometry</param>
+        /// <returns>Well-Known Text</returns>
+        public static string Write(ShapeData g)
+        {
             if (g.IsNull)
                 return "Null";
-			return Write(g, g.HasZ, g.HasM);
-		}
+            return Write(g, g.HasZ, g.HasM);
+        }
 
-		/// <summary>
-		/// Writes a <see cref="Geometry"/> instance as Well-Known Text according to the OGC Simple Features Specification 1.2.0.
-		/// </summary>
-		/// <param name="g">Geometry</param>
-		/// <param name="includeZ">Include Z values</param>
-		/// <param name="includeM">Include M values</param>
-		/// <returns>Well-Known Text</returns>
-		public static string Write(ShapeData g, bool includeZ, bool includeM)
-		{
-			StringBuilder sb = new StringBuilder();
-			WriteGeometry(g, sb, includeZ, includeM);
-			return sb.ToString();
-		}
+        /// <summary>
+        /// Writes a <see cref="Geometry"/> instance as Well-Known Text according to the OGC Simple Features Specification 1.2.0.
+        /// </summary>
+        /// <param name="g">Geometry</param>
+        /// <param name="includeZ">Include Z values</param>
+        /// <param name="includeM">Include M values</param>
+        /// <returns>Well-Known Text</returns>
+        public static string Write(ShapeData g, bool includeZ, bool includeM)
+        {
+            StringBuilder sb = new StringBuilder();
+            WriteGeometry(g, sb, includeZ, includeM);
+            return sb.ToString();
+        }
 
-		private static void WriteGeometry(ShapeData geometry, StringBuilder sb, bool includeZ, bool includeM)
-		{
+        private static void WriteGeometry(ShapeData geometry, StringBuilder sb, bool includeZ, bool includeM)
+        {
             var type = geometry.Type;
             sb.Append(type.ToString().ToUpperInvariant());
             if (geometry.IsEmpty)
@@ -48,22 +48,22 @@ namespace Microsoft.SqlServer.Types
                 return;
             }
             else sb.Append(" (");
-			if (type == OGCGeometryType.Point)
-				WritePoint(geometry, sb, includeZ, includeM);
-			else if (type == OGCGeometryType.LineString)
-				WriteLineString(geometry, sb, includeZ, includeM);
-			else if (type == OGCGeometryType.Polygon)
-				WritePolygon(geometry, sb, includeZ, includeM);
-			else if (type == OGCGeometryType.MultiPoint)
-				WriteMultiPoint(geometry, sb, includeZ, includeM);
-			else if (type == OGCGeometryType.MultiLineString)
-				WriteMultiLineString(geometry, sb, includeZ, includeM);
-			else if (type == OGCGeometryType.MultiPolygon)
-				WriteMultiPolygon(geometry, sb, includeZ, includeM);
-			else if (type == OGCGeometryType.GeometryCollection)
-				WriteGeometryCollection(geometry, sb, includeZ, includeM);
-			else
-				throw new ArgumentException("Invalid Geometry Type");
+            if (type == OGCGeometryType.Point)
+                WritePoint(geometry, sb, includeZ, includeM);
+            else if (type == OGCGeometryType.LineString)
+                WriteLineString(geometry, sb, includeZ, includeM);
+            else if (type == OGCGeometryType.Polygon)
+                WritePolygon(geometry, sb, includeZ, includeM);
+            else if (type == OGCGeometryType.MultiPoint)
+                WriteMultiPoint(geometry, sb, includeZ, includeM);
+            else if (type == OGCGeometryType.MultiLineString)
+                WriteMultiLineString(geometry, sb, includeZ, includeM);
+            else if (type == OGCGeometryType.MultiPolygon)
+                WriteMultiPolygon(geometry, sb, includeZ, includeM);
+            else if (type == OGCGeometryType.GeometryCollection)
+                WriteGeometryCollection(geometry, sb, includeZ, includeM);
+            else
+                throw new ArgumentException("Invalid Geometry Type");
             sb.Append(")");
         }
 
@@ -72,43 +72,43 @@ namespace Microsoft.SqlServer.Types
             WriteCoordinate(point.GetPointN(1), sb, includeZ, includeM);
         }
 
-		private static void WriteMultiPoint(ShapeData points, StringBuilder sb, bool includeZ, bool includeM)
-		{
-			for (int i = 0; i < points.NumPoints; i++)
-			{
-				if (i > 0) sb.Append(",");
-				WriteCoordinate(points.GetPointN(i+1), sb, includeZ, includeM);
-			}
-		}
+        private static void WriteMultiPoint(ShapeData points, StringBuilder sb, bool includeZ, bool includeM)
+        {
+            for (int i = 0; i < points.NumPoints; i++)
+            {
+                if (i > 0) sb.Append(",");
+                WriteCoordinate(points.GetPointN(i+1), sb, includeZ, includeM);
+            }
+        }
 
-		private static void WriteLineString(ShapeData line, StringBuilder sb, bool includeZ, bool includeM)
-		{
-			WriteCoordinateCollection(GetVertices(line), sb, includeZ, includeM);			
-		}
+        private static void WriteLineString(ShapeData line, StringBuilder sb, bool includeZ, bool includeM)
+        {
+            WriteCoordinateCollection(GetVertices(line), sb, includeZ, includeM);            
+        }
 
-		private static void WriteMultiLineString(ShapeData lines, StringBuilder sb, bool includeZ, bool includeM)
-		{
+        private static void WriteMultiLineString(ShapeData lines, StringBuilder sb, bool includeZ, bool includeM)
+        {
             sb.Append('(');
             for (int i = 0; i < lines.NumGeometries; i++)
-			{
+            {
                 if (i > 0) sb.Append("),(");
-				WriteCoordinateCollection(GetVertices(lines.GetRing(i)), sb, includeZ, includeM);
+                WriteCoordinateCollection(GetVertices(lines.GetRing(i)), sb, includeZ, includeM);
             }
             sb.Append(")");
         }
 
         private static void WritePolygon(ShapeData polygon, StringBuilder sb, bool includeZ, bool includeM)
-		{
-			WritePolygonContents(polygon, sb, includeZ, includeM);
-		}
+        {
+            WritePolygonContents(polygon, sb, includeZ, includeM);
+        }
 
         private static IEnumerable<PointZM> GetVertices(ShapeData g)
         {
             return Enumerable.Range(1, g.NumPoints).Select(s => g.GetPointN(s));
         }
 
-		private static void WritePolygonContents(ShapeData polygon, StringBuilder sb, bool includeZ, bool includeM)
-		{
+        private static void WritePolygonContents(ShapeData polygon, StringBuilder sb, bool includeZ, bool includeM)
+        {
             sb.Append('(');
             WriteCoordinateCollection(GetVertices(polygon.GetRing(0)), sb, includeZ, includeM);
             for (int i = 0; i < polygon.NumInteriorRing; i++)
@@ -120,7 +120,7 @@ namespace Microsoft.SqlServer.Types
         }
 
         private static void WriteMultiPolygon(ShapeData polys, StringBuilder sb, bool includeZ, bool includeM)
-		{
+        {
             sb.Append('(');
             for (int i = 0; i < polys.NumGeometries; i++)
             {
@@ -130,29 +130,29 @@ namespace Microsoft.SqlServer.Types
             sb.Append(')');
         }
 
-		private static void WriteGeometryCollection(ShapeData geoms, StringBuilder sb, bool includeZ, bool includeM)
-		{
-			for (int i = 0; i < geoms.NumGeometries; i++)
-			{
-				if (i > 0) sb.Append(",");
-				WriteGeometry(geoms.GetGeometryN(i+1), sb, includeZ, includeM);
-			}
-		}
+        private static void WriteGeometryCollection(ShapeData geoms, StringBuilder sb, bool includeZ, bool includeM)
+        {
+            for (int i = 0; i < geoms.NumGeometries; i++)
+            {
+                if (i > 0) sb.Append(",");
+                WriteGeometry(geoms.GetGeometryN(i+1), sb, includeZ, includeM);
+            }
+        }
 
-		private static void WriteCoordinateCollection(IEnumerable<PointZM> coords, StringBuilder sb, bool includeZ, bool includeM)
-		{
+        private static void WriteCoordinateCollection(IEnumerable<PointZM> coords, StringBuilder sb, bool includeZ, bool includeM)
+        {
             bool firstItem = true;
-			foreach(var c in coords)
-			{
+            foreach(var c in coords)
+            {
                 if (firstItem) firstItem = false;
                 else sb.Append(", ");
-				WriteCoordinate(c, sb, includeZ, includeM);
-			}
-		}
+                WriteCoordinate(c, sb, includeZ, includeM);
+            }
+        }
 
-		private static void WriteCoordinate(PointZM coord, StringBuilder sb, bool includeZ, bool includeM)
-		{
-			sb.AppendFormat(CultureInfo.InvariantCulture, "{0} {1}", coord.X, coord.Y);
+        private static void WriteCoordinate(PointZM coord, StringBuilder sb, bool includeZ, bool includeM)
+        {
+            sb.AppendFormat(CultureInfo.InvariantCulture, "{0} {1}", coord.X, coord.Y);
             if (includeZ)
             {
                 if (!double.IsNaN(coord.Z))
@@ -164,6 +164,6 @@ namespace Microsoft.SqlServer.Types
                 if (!double.IsNaN(coord.M))
                     sb.AppendFormat(CultureInfo.InvariantCulture, " {0}", coord.M);
             }
-		}
-	}
+        }
+    }
 }
