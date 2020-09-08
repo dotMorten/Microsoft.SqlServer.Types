08b4fe16cbc50256a7506def943a80a340c69810
Morten Nielsen <mn@iter.dk>
2020-09-07 21:06:07 -0700

    Fix reference tests and add them to CI

--- src/Microsoft.SqlServer.Types.Tests/AssemblyLoader.cs
+++ src/Microsoft.SqlServer.Types.Tests/AssemblyLoader.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Microsoft.SqlServer.Types.Tests
@@ -12,6 +13,9 @@ namespace Microsoft.SqlServer.Types.Tests
         public static void TestInit(TestContext context)
         {
             Init();
+#if !NETCOREAPP
+            LoadNativeAssemblies();
+#endif
         }
 
         private static void Init()
@@ -27,5 +31,39 @@ namespace Microsoft.SqlServer.Types.Tests
                 return typeof(SqlGeography).Assembly;
             return null;
         }
+
+#if !NETCOREAPP
+    
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr LoadLibrary(string path);
+
+        /// <summary>
+        /// Loads the required native assemblies for the current architecture (x86 or x64)
+        /// </summary>
+        /// <param name="rootApplicationPath">
+        /// Root path of the current application. Use Server.MapPath(".") for ASP.NET applications
+        /// and AppDomain.CurrentDomain.BaseDirectory for desktop applications.
+        /// </param>
+        public static void LoadNativeAssemblies()
+        {
+            var nativeBinaryPath = IntPtr.Size > 4 ? "x64" : "x86";
+
+            LoadNativeAssembly(nativeBinaryPath, "msvcr120.dll");
+            LoadNativeAssembly(nativeBinaryPath, "SqlServerSpatial140.dll");
+        }
+
+        private static void LoadNativeAssembly(string nativeBinaryPath, string assemblyName)
+        {
+            var path = System.IO.Path.Combine(nativeBinaryPath, assemblyName);
+            var ptr = LoadLibrary(path);
+            if (ptr == IntPtr.Zero)
+            {
+                throw new Exception(string.Format(
+                    "Error loading {0} (ErrorCode: {1})",
+                    assemblyName,
+                    Marshal.GetLastWin32Error()));
+            }
+        }
+#endif
     }
 }
